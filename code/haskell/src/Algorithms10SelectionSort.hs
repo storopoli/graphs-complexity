fedde6f763991003dd2d9d04a4721c7efaa5c29f
Jose Storopoli <jose@storopoli.com>
2025-08-12 15:46:25 -0300

    feat(haskell): selection sort

--- code/haskell/src/Algorithms10SelectionSort.hs
+++ code/haskell/src/Algorithms10SelectionSort.hs
@@ -1,4 +1,30 @@
+{-# LANGUAGE NumericUnderscores #-}
+
 module Main where
 
+import Utils
+
+{- | Perform Selection Sort on a list.
+Time complexity: O(n²)
+Space complexity: O(1)
+-}
+selectionSort :: (Ord a) =>
+    -- | List to sort
+    [a] ->
+    -- | Sorted list
+    [a]
+selectionSort [] = []
+selectionSort xs = minElem : selectionSort remaining
+  where
+    minElem = minimum xs
+    remaining = removeFirst minElem xs
+
+    -- Remove the first occurrence of an element from a list
+    removeFirst :: (Eq a) => a -> [a] -> [a]
+    removeFirst _ [] = []
+    removeFirst y (x:xs')
+        | x == y = xs'
+        | otherwise = x : removeFirst y xs'
+
 main :: IO ()
-main = putStrLn "Selection Sort - TODO: Port from C/Zig"
+main = testSortingAlgorithm "Selection Sort" selectionSort [10_000, 20_000, 30_000, 40_000]
