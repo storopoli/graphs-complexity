d8d4cd09b2a7ff7327ed607ea58b5d02a36d9fe7
Jose Storopoli <jose@storopoli.com>
2025-08-12 16:07:10 -0300

    feat(haskell): graph dfs/bfs search

--- code/haskell/src/Algorithms07BfsSearch.hs
+++ code/haskell/src/Algorithms07BfsSearch.hs
@@ -1,4 +1,67 @@
+{-# LANGUAGE NumericUnderscores #-}
+
 module Main where
 
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Maybe (fromMaybe)
+import System.CPUTime
+import Text.Printf
+
+-- | Graph represented as adjacency list
+type Graph = Map.Map Int [Int]
+
+-- | Create a graph from a list of edges
+createGraph :: Int -> [(Int, Int)] -> Graph
+createGraph numVertices =
+    foldr addEdge initialGraph
+  where
+    initialGraph = Map.fromList [(i, []) | i <- [0..numVertices-1]]
+    addEdge (u, v) graph =
+        Map.adjust (v:) u $ Map.adjust (u:) v graph
+
+{- | Breadth-First Search algorithm.
+Time complexity: O(V + E)
+Space complexity: O(V)
+-}
+bfs :: Graph -> Int -> Set.Set Int
+bfs graph startVertex = bfsHelper [startVertex] Set.empty
+  where
+    bfsHelper [] visited = visited
+    bfsHelper (current:queue) visited
+        | current `Set.member` visited = bfsHelper queue visited
+        | otherwise =
+            let newVisited = Set.insert current visited
+                neighbors = fromMaybe [] (Map.lookup current graph)
+                unvisitedNeighbors = filter (`Set.notMember` newVisited) neighbors
+                newQueue = queue ++ unvisitedNeighbors
+            in bfsHelper newQueue newVisited
+
+-- | Create a chain graph (0-1-2-...-N-1)
+createChainGraph :: Int -> Graph
+createChainGraph n = createGraph n [(i, i+1) | i <- [0..n-2]]
+
+-- | Measure execution time of an action
+timeAction :: IO a -> IO (a, Double)
+timeAction action = do
+    start <- getCPUTime
+    result <- action
+    end <- getCPUTime
+    let time = fromIntegral (end - start) / 1e12
+    return (result, time)
+
+-- | Test BFS with different graph sizes
+testBfs :: Int -> IO ()
+testBfs n = do
+    let graph = createChainGraph n
+
+    (visitedNodes, timeSpent) <- timeAction $ return (bfs graph 0)
+
+    printf "BFS with N = %d\n" n
+    printf "Visited %d nodes\n" (Set.size visitedNodes)
+    printf "Time taken: %f seconds\n\n" timeSpent
+
 main :: IO ()
-main = putStrLn "BFS Search - TODO: Port from C/Zig"
+main = do
+    let nValues = [1_000, 5_000, 10_000, 20_000, 30_000]
+    mapM_ testBfs nValues
--- code/haskell/src/Algorithms08DfsSearch.hs
+++ code/haskell/src/Algorithms08DfsSearch.hs
@@ -1,4 +1,65 @@
+{-# LANGUAGE NumericUnderscores #-}
+
 module Main where
 
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Maybe (fromMaybe)
+import System.CPUTime
+import Text.Printf
+
+-- | Graph represented as adjacency list
+type Graph = Map.Map Int [Int]
+
+-- | Create a graph from a list of edges
+createGraph :: Int -> [(Int, Int)] -> Graph
+createGraph numVertices =
+    foldr addEdge initialGraph
+  where
+    initialGraph = Map.fromList [(i, []) | i <- [0..numVertices-1]]
+    addEdge (u, v) graph =
+        Map.adjust (v:) u $ Map.adjust (u:) v graph
+
+{- | Depth-First Search algorithm.
+Time complexity: O(V + E)
+Space complexity: O(V)
+-}
+dfs :: Graph -> Int -> Set.Set Int
+dfs graph startVertex = dfsHelper startVertex Set.empty
+  where
+    dfsHelper current visited
+        | current `Set.member` visited = visited
+        | otherwise =
+            let newVisited = Set.insert current visited
+                neighbors = fromMaybe [] (Map.lookup current graph)
+                unvisitedNeighbors = filter (`Set.notMember` newVisited) neighbors
+            in foldr dfsHelper newVisited unvisitedNeighbors
+
+-- | Create a chain graph (0-1-2-...-N-1)
+createChainGraph :: Int -> Graph
+createChainGraph n = createGraph n [(i, i+1) | i <- [0..n-2]]
+
+-- | Measure execution time of an action
+timeAction :: IO a -> IO (a, Double)
+timeAction action = do
+    start <- getCPUTime
+    result <- action
+    end <- getCPUTime
+    let time = fromIntegral (end - start) / 1e12
+    return (result, time)
+
+-- | Test DFS with different graph sizes
+testDfs :: Int -> IO ()
+testDfs n = do
+    let graph = createChainGraph n
+
+    (visitedNodes, timeSpent) <- timeAction $ return (dfs graph 0)
+
+    printf "DFS with N = %d\n" n
+    printf "Visited %d nodes\n" (Set.size visitedNodes)
+    printf "Time taken: %f seconds\n\n" timeSpent
+
 main :: IO ()
-main = putStrLn "DFS Search - TODO: Port from C/Zig"
+main = do
+    let nValues = [1_000, 5_000, 10_000, 20_000, 30_000]
+    mapM_ testDfs nValues
